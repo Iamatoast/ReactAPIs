2fc743adff1fb00aa665cecae8edaedcba652e82
Iamatoast <neuyv@protonmail.com>
2025-06-24 09:36:52 -0300

    idk maybe ort is blocking api requests?

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+  let age;
 
   useEffect(() => {
   	document.title = name;
   });
   const checkName = () => {
 	  setName(document.getElementById("nameInput").value);
-	  let URL = 'https://api.agify.io?name=' + name;
+	  let URL = `https://api.agify.io?name=${name}`;
 	  axios({
 		  method:'get',
-		  url: URL
-	  });
+		  url: URL,
+	          responseType: 'application/json, harset=utf-8'
+	  })
+		  .then(function (response){
+			age = response.data.name;
+		  });
   }
   return (
     <>
 	  <input id="nameInput" className="nameInput" type="text"/>
 	  <button onClick={checkName}>Comprobar nombre</button>
 	  <p>{name}</p>
-	  <p></p>
-	  <p></p>
-	  <p></p>
+	  <p>{age}</p>
+	  <p>{}</p>
+	  <p>{}</p>
     </>
   );
 }
