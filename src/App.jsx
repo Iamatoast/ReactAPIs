ddcc122b42e5a4800b590c0e0dad4e7b83b37540
Iamatoast <48385749@est.ort.edu.ar>
2025-07-08 09:31:01 -0300

    3 apis integradas

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { getName } from 'country-list';
 import './App.css';
 
 function App() {
   const [name, setName] = useState("");
-  let age;
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [nation, setNation] = useState("");
 
   useEffect(() => {
   	document.title = name;
   });
   const checkName = () => {
-	  setName(document.getElementById("nameInput").value);
-	  let URL = `https://api.agify.io?name=${name}`;
+	  let tempName = document.getElementById("nameInput").value;
+	  setName(tempName);
 	  axios({
 		  method:'get',
-		  url: URL,
+		  url: `https://api.agify.io?name=${tempName}`,
 	          responseType: 'application/json, harset=utf-8'
 	  })
-		  .then(function (response){
-			age = response.data.name;
+		  .then(res =>{
+			setAge(JSON.parse(res.data).age);
+		  });
+	  axios({
+		  method:'get',
+		  url: `https://api.genderize.io?name=${tempName}`,
+	          responseType: 'application/json, harset=utf-8'
+	  })
+		  .then(res =>{
+			setGender(JSON.parse(res.data).gender);
+		  });
+	  axios({
+		  method:'get',
+		  url: `https://api.nationalize.io/?name=${tempName}`,
+	          responseType: 'application/json, harset=utf-8'
+	  })
+		  .then(res =>{
+			setNation(JSON.parse(res.data).country[0].country_id);
 		  });
   }
   return (
     <>
 	  <input id="nameInput" className="nameInput" type="text"/>
-	  <button onClick={checkName}>Comprobar nombre</button>
-	  <p>{name}</p>
-	  <p>{age}</p>
-	  <p>{}</p>
-	  <p>{}</p>
+	  <button onClick={checkName}>Test Name</button>
+	  <p>Name: {name}</p>
+	  <p>Age: {age}</p>
+	  <p>Gender: {gender}</p>
+	  <p>Nationality: {getName(nation)}</p>
     </>
   );
 }
